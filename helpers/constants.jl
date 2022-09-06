7101446d22f3c2b556d0879ad2a9c96a70d308c6
Son <snpham02@gmail.com>
2022-09-06 08:32:12 -0500

    checkpoint for hw-1

---
+++ helpers/constants.jl
@@ -0,0 +1,18 @@
+# constants
+
+# earth
+mass_earth = 5.9722e24 # kg
+gm_earth = 398600.435436 # km3/s2
+sma_earth_wrtsun = 149598023 # km
+ecc_earth_wrtsun = 0.016708617
+
+# moon
+mass_moon = 0.07346e24 # kg
+gm_moon = 4902.800066 # km3/s2
+sma_moon_wrtearth = 384400 # km
+ecc_moon_wrtearth = 0.05490
+
+# sun
+mass_sun = 1988500e24 # kg
+gm_sun = 132712440041.93938 # km3/s2
+
