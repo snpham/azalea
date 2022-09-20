ccfb868fdc626960b561f90f744e713c57ff2b11
Son <snpham02@gmail.com>
2022-09-20 13:54:36 -0500

    checkpoint

--- helpers/constants.jl
+++ helpers/constants.jl
@@ -1,18 +1,23 @@
 # constants
 
+G_univ = 6.6743e-20 # km3/(kg)
+
 # earth
-mass_earth = 5.9722e24 # kg
-gm_earth = 398600.435436 # km3/s2
 sma_earth_wrtsun = 149598023 # km
 ecc_earth_wrtsun = 0.016708617
 
 # moon
-mass_moon = 0.07346e24 # kg
-gm_moon = 4902.800066 # km3/s2
 sma_moon_wrtearth = 384400 # km
 ecc_moon_wrtearth = 0.05490
 
-# sun
-mass_sun = 1988500e24 # kg
-gm_sun = 132712440041.93938 # km3/s2
+
+# GM [km3/s2]
+gm_sun = 132712440041.93938
+gm_earth = 398600.435436
+gm_moon = 4902.800066
+
+# masses [kg]
+# mass_sun = 1.98855e30 # kg
+# mass_earth = 5.97217e24
+# mass_moon = 0.07346e24
 
